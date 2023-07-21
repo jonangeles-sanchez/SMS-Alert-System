7993247851d1c9e624c9d39df148e37c047c9c6d
jonangeles-sanchez <jonathanas1337@gmail.com>
2023-07-21 10:46:47 -0400

    Remove bloat

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
-import { MatToolbarModule } from '@angular/material/toolbar';
-import { MatFormFieldModule } from '@angular/material/form-field'; // Add this line
-import { MatCardModule } from '@angular/material/card'; // Add this line
-import { MatInputModule } from '@angular/material/input'; // Add this line
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations'; // Add this line
 
 import { AppRoutingModule } from './app-routing.module';
@@ -26,10 +22,6 @@ import { SidebarComponent } from './components/sidebar/sidebar.component';
     BrowserModule,
     AppRoutingModule,
     FormsModule,
-    MatToolbarModule,
-    MatFormFieldModule, // Add this line
-    MatCardModule,
-    MatInputModule,
     BrowserAnimationsModule,
   ],
   providers: [],
