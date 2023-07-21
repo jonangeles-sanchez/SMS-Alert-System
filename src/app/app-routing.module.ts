e8e1fe344a06b88793e318702b3e67e6d211194f
jonangeles-sanchez <jonathanas1337@gmail.com>
2023-07-21 14:00:51 -0400

    Add path

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { RequestFormComponent } from './components/request-form/request-form.component';
+import { ViewRequestsComponent } from './components/view-requests/view-requests.component';
 
-const routes: Routes = [{ path: '', component: RequestFormComponent }];
+const routes: Routes = [
+  { path: '', component: RequestFormComponent },
+  { path: 'requests', component: ViewRequestsComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
