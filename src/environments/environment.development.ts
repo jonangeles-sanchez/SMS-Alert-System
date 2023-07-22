37db8b8ea88c031fbfce210d43230253790f64ec
jonangeles-sanchez <jonathanas1337@gmail.com>
2023-07-22 14:37:17 -0400

    Add variables to environment.dev

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,1 +1,5 @@
-export const environment = {};
+export const environment = {
+  production: false,
+  apiUrl: 'http://localhost:5242/api/v1/alert',
+  twilioUrl: 'http://localhost:5242/api/v1/notify',
+};
